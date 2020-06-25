a91039ff6ab3b05e275e055307d4dffa46f47235
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-06-25 20:48:35 +0530

    changes

--- snippet.sh
+++ snippet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 webroot=httpd
-read -e -p "Do you want to implement SSL with the site [y/n]: " -i "y" yn
+read -p "Do you want to implement SSL with the site [y/n]: " yn
 if [[ yn == 'y' ]]; then
     echo "Let me check your server configuraiton.."
     progress
