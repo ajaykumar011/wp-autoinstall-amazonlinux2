442b330b88e291e69a489ae8f779ca2af87c9632
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-06-25 20:43:49 +0530

    changes

--- snippet.sh
+++ snippet.sh
@@ -4,22 +4,12 @@ read -e -p "Do you want to implement SSL with the site [y/n]: " -i "y" yn
 if [[ yn == 'y' ]]; then
     echo "Let me check your server configuraiton.."
     progress
-    DIR=.git
     if [ $webroot == "httpd" ]; then
         echo "You are running apache: "
-        if [ ! -d $DIR ]; then
-        	echo "Your are not in correct directory. Directory check failed."
-	        exit 1
-        fi
         sh extras_for_httpd.sh
-    elif [ $webroot == "httpd" ]; then
+    else 
         echo "You are running Nginx: "
-        if [ ! -d $DIR ]; then
-        	echo "Your are not in correct directory. Directory check failed."
-	        exit 1
-        fi
         sh extras_for_nginx.sh
-    else
-        echo "Thank you.."
-    fi
-fi
+else
+    echo "Thank you.. Good bye.."
+fi    
--- wp_install_script.sh
+++ wp_install_script.sh
@@ -281,25 +281,16 @@ echo -e "\e[1;32m Great Work.. \e[0m"
 echo "=========================================================="
 echo "$(tput setaf 7)$(tput setab 6)---|-WP READY TO ROCK-|---$(tput sgr 0)"
 read -e -p "Do you want to implement SSL with the site [y/n]: " -i "y" yn
+read -e -p "Do you want to implement SSL with the site [y/n]: " -i "y" yn
 if [[ yn == 'y' ]]; then
     echo "Let me check your server configuraiton.."
     progress
-    DIR=.git
     if [ $webroot == "httpd" ]; then
         echo "You are running apache: "
-        if [ ! -d $DIR ]; then
-        	echo "Your are not in correct directory. Directory check failed."
-	        exit 1
-        fi
         sh extras_for_httpd.sh
-    elif [ $webroot == "httpd" ]; then
+    else 
         echo "You are running Nginx: "
-        if [ ! -d $DIR ]; then
-        	echo "Your are not in correct directory. Directory check failed."
-	        exit 1
-        fi
         sh extras_for_nginx.sh
-    else
-        echo "Thank you.."
-    fi
+else
+    echo "Thank you.. Good bye.."
 fi
